ad09616ffbe12bcc9f5a9be027794c04d5d1d5f3
Sean Stephenson <sms@pixar.com>
2015-01-10 14:45:46 -0800

    update comments for plot 1

--- plot1.R
+++ plot1.R
@@ -1,15 +1,32 @@
+###################################################################################################
 getGlobalActivePower <- function() {
+###################################################################################################	
+	
+	# import local data
 	importdata <- read.table("data/household_power_consumption.txt", header = T, sep = ";")
+
+	# re cast date
 	importdata$Date <- as.Date(importdata$Date, format = "%d/%m/%Y")
+	
+	# get rows from target range
 	df <- importdata[(importdata$Date == "2007-02-01") | (importdata$Date == "2007-02-02"), ]
+
+	# return global active power vector
 	df$Global_active_power <- as.numeric(as.character(df$Global_active_power))		
 }
 
+###################################################################################################
 plotGlobalActivePower <- function() {
+###################################################################################################
+
+	# get data and assign	
 	gl <- getGlobalActivePower()
+	
+	# open png file
 	png("plot1.png", width = 480, height = 480, units = "px")
 	hist(gl, col = "red1", xlab = "Global Active Power (kilowats)", ylab = "Frequency", main = "Global Active Power")	
 	dev.off()
 }
 
+# Rock and Roll
 plotGlobalActivePower()
