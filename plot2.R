09ebb9955da365071063ef2a8d3f45ffcb739c94
Sean Stephenson <sms@pixar.com>
2015-01-10 12:40:57 -0800

    add function to get df with timestamp

---
+++ plot2.R
@@ -0,0 +1,7 @@
+getGlobalActivePowerWithTime <- function() {
+	importdata <- read.table("data/household_power_consumption.txt", header = T, sep = ";")
+	importdata$Date <- as.Date(importdata$Date, format = "%d/%m/%Y")
+	df <- importdata[(importdata$Date == "2007-02-01") | (importdata$Date == "2007-02-02"), ]
+	df <- transform(df, timestamp = as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+	return(df)
+}
