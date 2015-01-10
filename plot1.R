4aa69c3bda2ebd532cbaf2426577a72b42e83fc1
Sean Stephenson <sms@pixar.com>
2015-01-10 12:00:50 -0800

    get global active power as vector

--- plot1.R
+++ plot1.R
@@ -1,6 +1,10 @@
-getData <- function() {
-	importdata  <- read.csv("data/household_power_consumption.txt", sep = ";",)
-	importdata  <- subset(importdata, as.Date(importdata$Date, format='%d/%m/%Y') >= as.Date('2007-02-01'))
-	importdata  <- subset(importdata, as.Date(importdata$Date, format='%d/%m/%Y') <= as.Date('2007-02-02'))
-	return(importdata)
+getGlobalActivePower <- function() {
+	importdata <- read.table("data/household_power_consumption.txt", header = T, sep = ";")
+	importdata$Date <- as.Date(importdata$Date, format = "%d/%m/%Y")
+	df <- importdata[(importdata$Date == "2007-02-01") | (importdata$Date == "2007-02-02"), ]
+	df$Global_active_power <- as.numeric(as.character(df$Global_active_power))		
+}
+
+plotGlobalActive <- function() {
+	gl <- getGlobalActivePower()
 }
