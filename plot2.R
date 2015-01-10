74bd33811c70080b5aaa0784f653465a8f8a31e7
Sean Stephenson <sms@pixar.com>
2015-01-10 14:42:18 -0800

    update comments for plot 2

--- plot2.R
+++ plot2.R
@@ -1,20 +1,39 @@
+###################################################################################################
 getGlobalActivePowerWithTime <- function() {
+###################################################################################################
+
+	# import local data	
 	importdata <- read.table("data/household_power_consumption.txt", header = T, sep = ";")
+
+	# re cast date
 	importdata$Date <- as.Date(importdata$Date, format = "%d/%m/%Y")
+
+	# get rows from target range
 	df <- importdata[(importdata$Date == "2007-02-01") | (importdata$Date == "2007-02-02"), ]
+
+	# add column for timestamp
 	df <- transform(df, timestamp = as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")	
+
+	# asssign variables to return
 	gl <- df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
 	ts <- df$timestamp
 	return(list(gl, ts))
 }
 
+###################################################################################################
 plotGlobalActivePowerWithTime <- function() {
+###################################################################################################
+
+	# get data and assign variables	
 	data <- getGlobalActivePowerWithTime()
 	gl <- data[[1]]
-	ts <- data[[2]]	
+	ts <- data[[2]]
+	
+	# open png file
 	png("plot2.png", width = 480, height = 480, units = "px")
 	plot(ts, gl, type = "l", xlab = "" , ylab = "Global Active Power (kilowats)")
 	dev.off()
 }
 
+# Rock and Roll
 plotGlobalActivePowerWithTime()
