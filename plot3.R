0454b8ee85a7f34d66c3384ab2b5b7862dbbe19f
Sean Stephenson <sms@pixar.com>
2015-01-10 13:39:22 -0800

    update order of ops

--- plot3.R
+++ plot3.R
@@ -15,12 +15,12 @@ plotSubmetering <- function() {
 	s1 <- data[[1]]
 	s2 <- data[[2]]
 	s3 <- data[[3]]
-	ts <- data[[4]]		
+	ts <- data[[4]]
+	png("plot3.png", width = 480, height = 480, units = "px")
 	plot(ts, s1, type = "l", xlab = "" , ylab = "Energy sub metering")
 	lines(ts, s2, col = "red1")
 	lines(ts, s3, col = "blue")
 	legend('topright', c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c('black', 'red', 'blue'))	
-	png("plot3.png", width = 480, height = 480, units = "px")
 	dev.off()
 }
 
