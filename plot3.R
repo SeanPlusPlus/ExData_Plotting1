0cc3985a3b301e91f70136989c851dfa0bd565b5
Sean Stephenson <sms@pixar.com>
2015-01-10 13:23:31 -0800

    stub out function for plotting sub metering

---
+++ plot3.R
@@ -0,0 +1,30 @@
+getSubmetering <- function() {
+	importdata <- read.table("data/household_power_consumption.txt", header = T, sep = ";")
+	importdata$Date <- as.Date(importdata$Date, format = "%d/%m/%Y")
+	df <- importdata[(importdata$Date == "2007-02-01") | (importdata$Date == "2007-02-02"), ]
+	df <- transform(df, timestamp = as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")	
+	s1 <- df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+	s2 <- df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+	s3 <- df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+	ts <- df$timestamp
+	return(list(s1, s2, s3, ts))
+}
+
+plotSubmetering <- function() {
+	data <- getSubmetering()
+	return(data)
+	
+	#s1 <- data[[1]]
+	#s2 <- data[[2]]
+	#s3 <- data[[3]]
+	#ts <- data[[4]]	
+	
+	#plot(ts, s1, type = "l", xlab = "" , ylab = "")
+	#lines(s2)
+	#lines(s3)
+	
+	# png("plot3.png", width = 480, height = 480, units = "px")
+	# dev.off()
+}
+
+#plotSubmetering()
