c9ae986183931e293ea1b991f54b85914f929e5b
Sean Stephenson <sms@pixar.com>
2015-01-10 11:12:10 -0800

    subset target date range

--- plot1.R
+++ plot1.R
@@ -1,3 +1,6 @@
 display <- function() {
-  df <- read.csv('household_power_consumption.txt')
+	importdata  <- read.csv("data/household_power_consumption.txt")
+	importdata  <- subset(importdata, as.Date(importdata$Date, format='%d/%m/%Y') >= as.Date('2007-02-01'))
+	importdata  <- subset(importdata, as.Date(importdata$Date, format='%d/%m/%Y') <= as.Date('2007-02-02'))
+	return(importdata)
 }
